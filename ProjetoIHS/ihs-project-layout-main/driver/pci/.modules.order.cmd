d82b5983ab4bd6e310b6cd6ac35386f8f3f06ab9
AlvaroNegromonte <acn3@cin.ufpe.br>
2025-03-20 14:42:59 -0300

    Revert "(re)sending base file"
    This reverts commit 1940a9789e345b1a5e6c22c355794379c842dbb9.

--- ProjetoIHS/ihs-project-layout-main/driver/pci/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/de2i150/Documentos/ProjetoIHS/ihs-project-layout-main/driver/pci/modules.order := {   echo /home/de2i150/Documentos/ProjetoIHS/ihs-project-layout-main/driver/pci/de2i-150.o; :; } > /home/de2i150/Documentos/ProjetoIHS/ihs-project-layout-main/driver/pci/modules.order
--- ProjetoIHS/ihs-project-layout-main/driver/pci/de2i-150.mod
+++
@@ -1,1 +0,0 @@
-/home/de2i150/Documentos/ProjetoIHS/ihs-project-layout-main/driver/pci/de2i-150.o
