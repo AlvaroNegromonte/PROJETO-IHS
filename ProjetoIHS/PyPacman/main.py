89f1aa2f870141358160e5468d74da0f1ac24cc7
Vinícius Sousa <122307110+Luizfsl@users.noreply.github.com>
2025-03-25 15:44:58 -0300

    Update main.py

--- ProjetoIHS/PyPacman/main.py
+++ ProjetoIHS/PyPacman/main.py
@@ -1,5 +1,14 @@
+import os, sys              #Modificacao
+from fcntl import ioctl
+from integracao import * 
+
 from src.runner import GameRun
 
 if __name__=='__main__':
     gr = GameRun()
-    gr.main()
+    
+    #Modificacao
+    if gr.io.get_SW(0): 
+        gr.main()
+        
+#Ou fzr self.io = IO() if io.get_sw(0): ...
