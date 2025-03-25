0466b6ce11a77cc0b2b9ff2696733922b3e00b4f
Vinícius Sousa <122307110+Luizfsl@users.noreply.github.com>
2025-03-25 16:50:38 -0300

    Update event_management.py

--- ProjetoIHS/PyPacman/src/game/event_management.py
+++ ProjetoIHS/PyPacman/src/game/event_management.py
@@ -2,7 +2,7 @@ from pygame import (K_DOWN, K_ESCAPE, K_LEFT, K_RIGHT, K_SPACE, K_UP, KEYDOWN,
                     QUIT, K_q)
 from pygame import USEREVENT
 from pygame.time import set_timer
-from ProjetoIHS.PyPacman.integracao import *
+from integracao import *
 
 class EventHandler:
     def __init__(self, screen, game_state):
